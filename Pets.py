e0727f68aa6d5820ec568cd2f64165178c95e1da
Unending <54435202+Unending@users.noreply.github.com>
2019-08-23 19:31:49 +0200

    scripts

---
+++ Pets.py
@@ -0,0 +1,85 @@
+# API details: https://habitica.com/#/options/settings/api
+# API docs: https://habitica.com/apidoc/
+
+import requests
+import ConfigParser
+
+config = ConfigParser.RawConfigParser()
+config.read("config.ini")
+
+HABITICA_USER = config.get("AUTH", "HABITICA_USER")
+HABITICA_TOKEN = config.get("AUTH", "HABITICA_TOKEN")
+auth_headers = {"x-api-user": HABITICA_USER, "x-api-key": HABITICA_TOKEN}
+
+r = requests.get("https://habitica.com/api/v4/user", headers=auth_headers)
+hatchingPotionsInventory = r.json()["data"]["items"]["hatchingPotions"]
+petsInventory = r.json()["data"]["items"]["pets"]
+eggsInventory = r.json()["data"]["items"]["eggs"]
+foodInventory = r.json()["data"]["items"]["food"]
+mountsInventory = r.json()["data"]["items"]["mounts"]
+foodPreferenceList = {"Base": "Meat", "White": "Milk", "Desert": "Potatoe", "Red": "Strawberry", "Shade": "Chocolate", "Skeleton": "Fish", "Zombie": "RottenMeat", "CottonCandyBlue": "CottonCandyBlue", "CottonCandyPink": "CottonCandyPink", "Golden": "Honey"}
+action = True
+
+
+petTypeList = {}
+for pets in petsInventory:
+    petType = pets.split("-")[0]
+    if petsInventory.get(pets) >= 5:
+        if petType in petTypeList:
+            petTypeList[petType] += 1
+        else:
+            petTypeList[petType] = 1
+
+
+while action is True:
+    action = False
+    for egg in eggsInventory:
+        for hatchingPotion in hatchingPotionsInventory:
+            pet = egg + "-" + hatchingPotion
+
+            # WHEN HATCHABLE
+            if eggsInventory.get(egg) > 0 and hatchingPotionsInventory.get(hatchingPotion) > 0:
+                # HATCH PETS
+                if pet not in petsInventory or petsInventory.get(pet) == -1:
+                    r = requests.post(
+                        "https://habitica.com/api/v3/user/hatch/" + egg + "/" + hatchingPotion,
+                        headers=auth_headers)
+                    if r.status_code == 200:
+                        action = True
+                        print(hatchingPotion + " " + egg + " hatched!")
+                        eggsInventory[egg] -= 1
+                        hatchingPotionsInventory[hatchingPotion] -= 1
+                        petsInventory[pet] = "5"
+                        if egg in petTypeList:
+                            petTypeList[egg] += 1
+                        else:
+                            petTypeList[egg] = 1
+                    if r.status_code >= 400:
+                        print("Failed to hatch " + hatchingPotion + " " + egg + ": " + r.json()["message"])
+
+                # FEED PETS
+                if petTypeList[egg] >= 10:
+                    for food in foodInventory:
+                        if pet not in mountsInventory and foodInventory.get(food) > 0 and 0 < petsInventory.get(pet) < 50:
+                            if hatchingPotion in foodPreferenceList:
+                                feed = foodPreferenceList.get(hatchingPotion)
+                                foodList = food.split("_")
+                                if len(foodList) > 1 and foodList[1] == hatchingPotion:
+                                    feed = food
+                            else:
+                                feed = max(foodPreferenceList, key=foodPreferenceList.get)
+
+                            r = requests.post(
+                                "https://habitica.com/api/v3/user/feed/" + pet + "/" + feed,
+                                headers=auth_headers)
+                            if r.status_code == 200:
+                                action = True
+                                rData = r.json()["data"]
+                                print(hatchingPotion + " " + egg + " fed with " + feed + ". Current value: " + str(rData))
+                                foodInventory[feed] -= 1
+                                petsInventory[pet] = rData
+                                petTypeList[egg] -= 1
+                                if rData == -1:
+                                    mountsInventory[pet] = "true"
+                            if r.status_code >= 400:
+                                print("Failed to feed " + hatchingPotion + " " + egg + ": " + r.json()["message"])
---
+++ Skills.py
@@ -0,0 +1,78 @@
+# API details: https://habitica.com/#/options/settings/api
+# API docs: https://habitica.com/apidoc/
+
+import requests
+import ConfigParser
+
+config = ConfigParser.RawConfigParser()
+config.read("config.ini")
+
+HABITICA_USER = config.get("AUTH", "HABITICA_USER")
+HABITICA_TOKEN = config.get("AUTH", "HABITICA_TOKEN")
+auth_headers = {"x-api-user": HABITICA_USER, "x-api-key": HABITICA_TOKEN}
+spellId = "fireball"
+
+
+def getUP():
+    return requests.get(
+        "https://habitica.com/api/v4/user",
+        headers=auth_headers)
+
+
+def getParty():
+    return requests.get(
+        "https://habitica.com/api/v3/groups/party",
+        headers=auth_headers)
+
+
+def targetId():
+    r = requests.get(
+        "https://habitica.com/api/v3/tasks/user?type=dailys",
+        headers=auth_headers)
+
+    tasks = {}
+    for task in r.json()["data"]:
+        tasks["{_id}".format(**task)] = float("{value}".format(**task))
+
+    return max(tasks, key=tasks.get)
+
+
+userMana = float(getUP().json()["data"]["stats"]["mp"])
+quest = getParty().json()["data"]["quest"]["key"]
+
+
+def pendingDMG():
+    return float(getUP().json()["data"]["party"]["quest"]["progress"]["up"])
+
+
+def bossHP():
+    return float(getParty().json()["data"]["quest"]["progress"]["hp"])
+
+
+# # QUEST CAST SPELLS
+if quest != "" and not getParty().json()["data"]["quest"]["progress"]["collect"]:
+    while (userMana >= 10 and (bossHP() - pendingDMG() > 0)):
+        r = requests.post(
+            "https://habitica.com/api/v4/user/class/cast/" + spellId + "?targetId=" + targetId(),
+            headers=auth_headers)
+        if r.status_code == 200:
+            print("Casted spell \"" + spellId + "\"" + " (boss damage)")
+            userMana -= 10
+        if r.status_code >= 400:
+            print("Failed to cast spell: " + r.json()["message"])
+    print("Boss HP remaining: " + str(max(0, round((bossHP() - pendingDMG())))))
+
+# MANA BURNOFF CAST SPELLS
+maxMP = float(getUP().json()["data"]["stats"]["maxMP"])
+
+while userMana > maxMP * 0.9:
+    r = requests.post(
+        "https://habitica.com/api/v4/user/class/cast/" + spellId + "?targetId=" + targetId(),
+        headers=auth_headers)
+    if r.status_code == 200:
+        print("Casted spell \"" + spellId + "\"" + " (mana burnoff)")
+        userMana -= 10
+    if r.status_code >= 400:
+        print("Failed to cast spell: " + r.json()["message"])
+
+print("Mana remaining: " + str(round(userMana)))
