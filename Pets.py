d7fc9e0376440e1af428c0cb3fe35d31b37e7170
Unending <54435202+Unending@users.noreply.github.com>
2019-11-07 09:44:46 +0100

    junk

--- Pets.py
+++ Pets.py
@@ -31,54 +31,50 @@ for pets in petsInventory:
             petTypeList[petType] = 1
 
 
-while action is True:
-    action = False
-    for egg in eggsInventory:
-        for hatchingPotion in hatchingPotionsInventory:
-            pet = egg + "-" + hatchingPotion
+for egg in eggsInventory:
+    for hatchingPotion in hatchingPotionsInventory:
+        pet = egg + "-" + hatchingPotion
 
-            # WHEN HATCHABLE
-            if eggsInventory.get(egg) > 0 and hatchingPotionsInventory.get(hatchingPotion) > 0:
-                # HATCH PETS
-                if pet not in petsInventory or petsInventory.get(pet) == -1:
-                    r = requests.post(
-                        "https://habitica.com/api/v3/user/hatch/" + egg + "/" + hatchingPotion,
-                        headers=auth_headers)
-                    if r.status_code == 200:
-                        action = True
-                        print(hatchingPotion + " " + egg + " hatched!")
-                        eggsInventory[egg] -= 1
-                        hatchingPotionsInventory[hatchingPotion] -= 1
-                        petsInventory[pet] = "5"
-                        if egg in petTypeList:
-                            petTypeList[egg] += 1
+        # WHEN HATCHABLE
+        if eggsInventory.get(egg) > 0 and hatchingPotionsInventory.get(hatchingPotion) > 0:
+            # HATCH PETS
+            if pet not in petsInventory or petsInventory.get(pet) == -1:
+                r = requests.post(
+                    "https://habitica.com/api/v3/user/hatch/" + egg + "/" + hatchingPotion,
+                    headers=auth_headers)
+                if r.status_code == 200:
+                    print(hatchingPotion + " " + egg + " hatched!")
+                    eggsInventory[egg] -= 1
+                    hatchingPotionsInventory[hatchingPotion] -= 1
+                    petsInventory[pet] = "5"
+                    if egg in petTypeList:
+                        petTypeList[egg] += 1
+                    else:
+                        petTypeList[egg] = 1
+                if r.status_code >= 400:
+                    print("Failed to hatch " + hatchingPotion + " " + egg + ": " + r.json()["message"])
+
+            # FEED PETS
+            if petTypeList[egg] >= 10:
+                for food in foodInventory:
+                    if pet not in mountsInventory and foodInventory.get(food) > 0 and 0 < petsInventory.get(pet) < 50:
+                        if hatchingPotion in foodPreferenceList:
+                            feed = foodPreferenceList.get(hatchingPotion)
+                            foodList = food.split("_")
+                            if len(foodList) > 1 and foodList[1] == hatchingPotion:
+                                feed = food
                         else:
-                            petTypeList[egg] = 1
-                    if r.status_code >= 400:
-                        print("Failed to hatch " + hatchingPotion + " " + egg + ": " + r.json()["message"])
-
-                # FEED PETS
-                if petTypeList[egg] >= 10:
-                    for food in foodInventory:
-                        if pet not in mountsInventory and foodInventory.get(food) > 0 and 0 < petsInventory.get(pet) < 50:
-                            if hatchingPotion in foodPreferenceList:
-                                feed = foodPreferenceList.get(hatchingPotion)
-                                foodList = food.split("_")
-                                if len(foodList) > 1 and foodList[1] == hatchingPotion:
-                                    feed = food
-                            else:
-                                feed = max(foodInventory, key=foodInventory.get)
-                            r = requests.post(
-                                "https://habitica.com/api/v3/user/feed/" + pet + "/" + feed,
-                                headers=auth_headers)
-                            if r.status_code == 200:
-                                action = True
-                                rData = r.json()["data"]
-                                print(hatchingPotion + " " + egg + " fed with " + feed + ". Current value: " + str(rData))
-                                foodInventory[feed] -= 1
-                                petsInventory[pet] = rData
-                                petTypeList[egg] -= 1
-                                if rData == -1:
-                                    mountsInventory[pet] = "true"
-                            if r.status_code >= 400:
-                                print("Failed to feed " + hatchingPotion + " " + egg + ": " + r.json()["message"])
+                            feed = max(foodInventory, key=foodInventory.get)
+                        r = requests.post(
+                            "https://habitica.com/api/v3/user/feed/" + pet + "/" + feed,
+                            headers=auth_headers)
+                        if r.status_code == 200:
+                            rData = r.json()["data"]
+                            print(hatchingPotion + " " + egg + " fed with " + feed + ". Current value: " + str(rData))
+                            foodInventory[feed] -= 1
+                            petsInventory[pet] = rData
+                            petTypeList[egg] -= 1
+                            if rData == -1:
+                                mountsInventory[pet] = "true"
+                        if r.status_code >= 400:
+                            print("Failed to feed " + hatchingPotion + " " + egg + ": " + r.json()["message"])
