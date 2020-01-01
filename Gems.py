2861cee1d6721e42f80d3dd49189663080ea7eba
Unending <54435202+Unending@users.noreply.github.com>
2020-01-01 11:51:48 +0100

    gems

---
+++ Gems.py
@@ -0,0 +1,19 @@
+# API details: https://habitica.com/#/options/settings/api
+# API docs: https://habitica.com/apidoc/
+
+import requests
+import ConfigParser
+
+config = ConfigParser.RawConfigParser()
+config.read("config.ini")
+
+HABITICA_USER = config.get("AUTH", "HABITICA_USER")
+HABITICA_TOKEN = config.get("AUTH", "HABITICA_TOKEN")
+auth_headers = {"x-api-user": HABITICA_USER, "x-api-key": HABITICA_TOKEN}
+
+returnMessage = "+1 Gem"
+
+while returnMessage == "+1 Gem":
+    r = requests.post("https://habitica.com/api/v3/user/purchase/gems/gem", headers=auth_headers)
+    returnMessage = r.json()['message']
+    print(returnMessage)
